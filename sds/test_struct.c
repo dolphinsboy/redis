fa005b1b590622da31c9d2a6eb061fb79b1247aa
dolphinsboy <sguo421@gmail.com>
2016-04-06 11:00:14 +0800

    test sds structure memory layout

---
+++ sds/test_struct.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct sdshdr{
+    int len;
+    int free;
+    int c;
+    char buf[];
+};
+
+int main(int argc, char **argv){
+
+    struct sdshdr *sh;
+    char *p = "Helloworld";
+
+    sh = malloc(sizeof(struct sdshdr));
+
+    sh->len = 10;
+    sh->free = 0;
+    sh->c = 2;
+
+    memcpy(sh->buf, p, strlen(p));
+    sh->buf[strlen(p)+1] = '\0';
+
+    char *pt = (char*)sh->buf;
+
+    struct sdshdr *psh = (void*)(pt-sizeof(struct sdshdr));
+
+    //printf("len = %ld,free = %ld,buf = %ld, psh=%ld, c = %ld\n", &(psh->len), &(psh->free), &(psh->buf), psh, &(psh->c));
+    printf("len = %ld,free = %ld,buf = %ld, psh=%ld\n", &(psh->len), &(psh->free), &(psh->buf), psh);
+    printf("%d,%d,%s, %ld\n", psh->len, psh->free, psh->buf, psh);
+    printf("%d,%d,%d,%s, %ld\n", psh->len, psh->c, psh->free, psh->buf, psh);
+
+    return 0;
+}
